70ba8f66fde5a21e9d92ecc5cac42557b5ed5a09
Ivan Andreev <iivvaann.andreev@gmail.com>
2021-02-09 01:51:34 +0200

    Can move platform

--- Assets/Scripts/Behaviours/Player/CharacterController2D.cs
+++ Assets/Scripts/Behaviours/Player/CharacterController2D.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 1;
+    
+    private Rigidbody2D _rigidBody;
 
+    private void Awake()
+    {
+        _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void OnMove(InputValue input)
+    {
+        Move(input.Get<Vector2>());
+    }
+
+    public void Move(Vector2 direction)
+    {
+        Vector2 temp = _rigidBody.velocity;
+        temp.x = direction.x * speed;
+        _rigidBody.velocity = temp;
     }
 }
