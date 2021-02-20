4fead5f69cfec9043a419557aac2532dd88ea8f6
Ivan Andreev <iivvaann.andreev@gmail.com>
2021-02-20 23:14:09 +0200

    Ball bounces off walls
    Use transform.position to move rather than velocity

--- Assets/Scripts/Behaviours/Ball/BallMovement2D.cs
+++ Assets/Scripts/Behaviours/Ball/BallMovement2D.cs
@@ -2,29 +2,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class BallMovement2D : MonoBehaviour
 {
     [SerializeField] private float speed = 1;
 
     private Vector2 _movementDirection;
-    private Rigidbody2D _rigidbody;
+    private BoxCollider2D _boxCollider;
 
+    private float _leftRight;
+    private float _bottomTop;
+    
+    
     private void Awake()
     {
-        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _boxCollider = gameObject.GetComponent<BoxCollider2D>();
         _movementDirection = new Vector2(-1, -1);
-        Movement();
+
+        float screenHeight = Camera.main.orthographicSize;
+        float screenWidth = screenHeight * Camera.main.aspect;
+
+        _bottomTop = screenHeight - (_boxCollider.size.y / 2);
+        _leftRight = screenWidth - (_boxCollider.size.x / 2);
     }
 
-    public void Movement()
-    {
-        _rigidbody.velocity = _movementDirection * speed;
-    }
+    
+    
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 toMove = _movementDirection * (speed * Time.deltaTime);
+        Vector3 newPos = new Vector3(toMove.x, toMove.y, 0) + transform.position;
+
+        if (Math.Abs(newPos.y) >= _bottomTop)
+        {
+            _movementDirection.y *= -1;
+        }
+        else if (Math.Abs(newPos.x) >= _leftRight)
+        {
+            _movementDirection.x *= -1;
+        }
+        else
+        {
+            transform.position = newPos;
+        }
     }
+    
+    
+    public void MoveBall()
+    {
+        Debug.Log("Set movement: " + _movementDirection);
+    }
+   
 }
