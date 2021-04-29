0551796497921a4eeff0e53c97acb560b3aab162
Ivan Andreev <iivvaann.andreev@gmail.com>
2021-04-29 11:47:23 +0100

    colourful and multi-hit tiles

--- Assets/Scripts/Behaviours/Tiles/TileBehaviour2D.cs
+++ Assets/Scripts/Behaviours/Tiles/TileBehaviour2D.cs
@@ -1,46 +1,82 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 [System.Serializable]
 public class TileDestroyEvent : UnityEvent<int> {}
 
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class TileBehaviour2D : MonoBehaviour
 {
     public static TileDestroyEvent TileDestroyEvent = new TileDestroyEvent();
+
+    private SpriteRenderer _spriteRenderer;
     
-    private int _numStrikesToDisappear = 1;
-    private int _numStrikesLeft = 1;
-    
+    private int _numStrikesToDisappear;
+    private int _numStrikesLeft;
+ 
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        NumStrikesToDisappear = Random.Range(1, 3+1);
+        // NumStrikesToDisappear = 1; use for faster debug
+    }
+
     public void OnCollisionExit2D(Collision2D other)
     {
         NumStrikesLeft--;
         if (NumStrikesLeft == 0)
         {
-            TileDestroyEvent.Invoke(_numStrikesToDisappear);
+            TileDestroyEvent.Invoke(NumStrikesToDisappear);
             Destroy(gameObject);
         }
     }
 
-    public int NumStrikesToDisappear
+    private int NumStrikesToDisappear
     {
         get => _numStrikesToDisappear;
         set
         {
             _numStrikesToDisappear = value;
             NumStrikesLeft = _numStrikesToDisappear;
-            // numStrikesChanged() event to change tile colour; or do a numStrikes class
         }
     }
 
-    public int NumStrikesLeft
+    private int NumStrikesLeft
     {
         get => _numStrikesLeft;
         set
         {
             _numStrikesLeft = value;
-            // Call NumStrikesChanged (value)
+            NumStrikesLeftChanged();
         }
+    }
+
+    private void NumStrikesLeftChanged()
+    {
+        Color color;
+        
+        switch (NumStrikesLeft)
+        {
+            case 3:
+                color = Color.red;
+                break;
+            case 2:
+                color = Color.yellow;
+                break;
+            case 1:
+                color = Color.green;
+                break;
+            case 0:
+                color = Color.white;
+                break;
+            default:
+                NumStrikesLeft = 1;
+                return;
+        }
+
+        _spriteRenderer.color = color;
     }
 }
