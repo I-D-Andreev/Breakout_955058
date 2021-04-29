4cb86adfcd1432e2dc4422019633d8040d5e293f
Ivan Andreev <iivvaann.andreev@gmail.com>
2021-04-29 10:28:29 +0100

    Score update default script

---
+++ Assets/Scripts/Behaviours/TextBehaviours/ScoreUpdateListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class ScoreUpdateListener : MonoBehaviour
+{   
+    private TMP_Text scoreText;
+    private ulong score;
+    
+    void Awake()
+    {
+        score = 0;
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
