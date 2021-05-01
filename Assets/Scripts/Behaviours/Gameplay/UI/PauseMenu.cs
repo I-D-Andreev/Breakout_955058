73868a081eb4ca53a434598f784614ddd37d77da
Ivan Andreev <iivvaann.andreev@gmail.com>
2021-05-01 18:11:32 +0100

    Sample Pause Menu

---
+++ Assets/Scripts/Behaviours/Gameplay/UI/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    
+    private bool _isGamePaused = false;
+    
+  
+  
+    public void OnPause(InputValue _)
+    {
+        Debug.Log("Pause");
+        Debug.Log(_isGamePaused);
+        
+        if (_isGamePaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    private void ResumeGame()
+    {
+        menu.SetActive(false);
+        _isGamePaused = false;
+        Time.timeScale = 1;
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        menu.SetActive(true);
+        _isGamePaused = true;
+    }
+}
