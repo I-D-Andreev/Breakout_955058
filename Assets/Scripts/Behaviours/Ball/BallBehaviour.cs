80d43a4327f6e025b78bd9d9a7fcd5a5dc70a05a
Ivan Andreev <iivvaann.andreev@gmail.com>
2021-05-01 17:11:08 +0100

    Send event on death

---
+++ Assets/Scripts/Behaviours/Ball/BallBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class BallBehaviour : MonoBehaviour
+{
+    public static UnityEvent BallDeathEvent = new UnityEvent();
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            BallDeathEvent.Invoke();
+        }
+    }
+}
