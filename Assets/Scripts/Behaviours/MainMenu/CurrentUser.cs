351546aeabe951732de30ff58d9cc2c5120df658
Ivan Andreev <iivvaann.andreev@gmail.com>
2021-05-05 00:38:59 +0100

    Show current user in main menu

---
+++ Assets/Scripts/Behaviours/MainMenu/CurrentUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CurrentUser : MonoBehaviour
+{
+    private TextMeshProUGUI _textBox;
+
+    private void Awake()
+    {
+        _textBox = gameObject.GetComponent<TextMeshProUGUI>();
+        _textBox.text = $"User: {Database.GameData.LoggedInProfile.PlayerName}";
+    }
+}
